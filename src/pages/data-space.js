6c9ef3887ba3bff5fa23879317dfa862b3153229
Allen2730 <1051072440@qq.com>
2025-02-17 17:51:07 +0800

    add send token to plugins

--- src/pages/data-space.js
+++ src/pages/data-space.js
@@ -1,11 +1,9 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { useRef, useState, useEffect } from "react"
-import { useAccount, useSignMessage, useAccountEffect, useDisconnect } from 'wagmi'
+import { useAccount } from 'wagmi'
 
-export default function DataSpace () {
-    const router = useRouter()
-
+export default function DataSpace() {
     const menu = useRef(null)
     const dataSpaceButton = useRef(null)
     const taskButton = useRef(null)
@@ -14,7 +12,7 @@ export default function DataSpace () {
     const dataSpace = useRef(null)
     const task = useRef(null)
     const [isCheckedIn, setIsCheckedIn] = useState(false)
-    const { isConnected, address } = useAccount();
+    const { address } = useAccount();
 
     const [time, setTime] = useState(null); // State to store time left
     const [hours, setHours] = useState(0);
@@ -23,7 +21,7 @@ export default function DataSpace () {
 
     useEffect(() => {
         if (time === null) return; // Exit if time hasn't been fetched yet
-    
+
         const interval = setInterval(() => {
             setTime((prevTime) => {
                 if (prevTime <= 0) {
@@ -33,19 +31,19 @@ export default function DataSpace () {
                 return prevTime - 1; // Decrement time by 1 second
             });
         }, 1000);
-    
+
         return () => clearInterval(interval); // Cleanup on component unmount
     }, [time]);
-    
+
     useEffect(() => {
         if (time === null) return;
 
         if (time !== null) {
-            const hours = time/3600
-            const minutes = (hours - parseInt(hours))*60
-            const seconds = (minutes - parseInt(minutes))*60
+            const hours = time / 3600
+            const minutes = (hours - parseInt(hours)) * 60
+            const seconds = (minutes - parseInt(minutes)) * 60
 
-    
+
             setHours(parseInt(hours));
             setMinutes(parseInt(minutes));
             setSeconds(parseInt(seconds));
@@ -84,7 +82,7 @@ export default function DataSpace () {
                 <button className="bg-x-green px-4 py-2 rounded-full text-black text-xs">Refer your Friends</button>
             </div>
 
-            <p className="w-full bg-[#021B12] p-4 rounded-xl font-bold mt-4 truncate">{ address }</p>
+            <p className="w-full bg-[#021B12] p-4 rounded-xl font-bold mt-4 truncate">{address}</p>
 
             <div className="w-full bg-[#021B12] p-4 rounded-xl font-bold">
                 <div className="grid grid-cols-2">
@@ -105,11 +103,11 @@ export default function DataSpace () {
 
             <div className="mt-4 flex flex-col gap-4">
                 <div ref={menu} className="flex gap-4">
-                    <button ref={ dataSpaceButton } onClick={ toggleSection } className="px-2 pb-1 border-b-2 border-solid border-x-green text-x-green text-sm">Data Space</button>
-                    <button ref={ taskButton } onClick={ toggleSection } className="px-2 pb-1 border-b-0 border-solid border-x-green text-sm text-white">Task</button>
+                    <button ref={dataSpaceButton} onClick={toggleSection} className="px-2 pb-1 border-b-2 border-solid border-x-green text-x-green text-sm">Data Space</button>
+                    <button ref={taskButton} onClick={toggleSection} className="px-2 pb-1 border-b-0 border-solid border-x-green text-sm text-white">Task</button>
                 </div>
 
-                <div ref={ dataSpace } className="flex flex-col gap-4">
+                <div ref={dataSpace} className="flex flex-col gap-4">
                     <div className="w-full bg-[#021B12] p-6 rounded-xl flex items-center justify-between gap-2">
                         <div className="flex flex-col gap-2">
                             <h2 className="text-white text-lg font-bold">Storage Units</h2>
@@ -136,14 +134,14 @@ export default function DataSpace () {
                     </div>
                 </div>
 
-                <div ref={ task } className="hidden flex-col gap-4">
+                <div ref={task} className="hidden flex-col gap-4">
                     <div className="w-full bg-[#021B12] p-6 rounded-xl flex items-center justify-between gap-2">
                         <div className="flex flex-col gap-2">
                             <h2 className="text-white text-lg font-bold">Check In</h2>
                             <p className="text-xs">Continuous sign-in will earn extra points.</p>
-                            { isCheckedIn? <p className="text-xs text-x-green flex gap-2 items-center"><svg xmlns="http://www.w3.org/2000/svg" className="size-4" height="48px" viewBox="0 -960 960 960" width="48px" fill="#05f292"><path d="M479-82q-74 0-139.5-28t-114-76.5q-48.5-48.5-77-114T120-440.73q0-74.74 28.5-140Q177-646 225.5-695t114-77Q405-800 479-800t139.5 28Q684-744 733-695t77 114.27q28 65.26 28 140 0 74.73-28 140.23-28 65.5-77 114T618.5-110Q553-82 479-82Zm0-357Zm121 161 42-42-130-130v-190h-60v214l148 148ZM214-867l42 42L92-667l-42-42 164-158Zm530 0 164 158-42 42-164-158 42-42ZM479.04-142Q604-142 691-229.04q87-87.05 87-212Q778-566 690.96-653q-87.05-87-212-87Q354-740 267-652.96q-87 87.05-87 212Q180-316 267.04-229q87.05 87 212 87Z"/></svg> { hours }h: { minutes }min: { seconds }s</p>: null }
+                            {isCheckedIn ? <p className="text-xs text-x-green flex gap-2 items-center"><svg xmlns="http://www.w3.org/2000/svg" className="size-4" height="48px" viewBox="0 -960 960 960" width="48px" fill="#05f292"><path d="M479-82q-74 0-139.5-28t-114-76.5q-48.5-48.5-77-114T120-440.73q0-74.74 28.5-140Q177-646 225.5-695t114-77Q405-800 479-800t139.5 28Q684-744 733-695t77 114.27q28 65.26 28 140 0 74.73-28 140.23-28 65.5-77 114T618.5-110Q553-82 479-82Zm0-357Zm121 161 42-42-130-130v-190h-60v214l148 148ZM214-867l42 42L92-667l-42-42 164-158Zm530 0 164 158-42 42-164-158 42-42ZM479.04-142Q604-142 691-229.04q87-87.05 87-212Q778-566 690.96-653q-87.05-87-212-87Q354-740 267-652.96q-87 87.05-87 212Q180-316 267.04-229q87.05 87 212 87Z" /></svg> {hours}h: {minutes}min: {seconds}s</p> : null}
                         </div>
-                        <button ref={ checkInButton } onClick={ toggleCheckIn } className="rounded-full px-4 py-1.5 text-sm bg-x-green text-black w-fit text-nowrap">Check In</button>
+                        <button ref={checkInButton} onClick={toggleCheckIn} className="rounded-full px-4 py-1.5 text-sm bg-x-green text-black w-fit text-nowrap">Check In</button>
                     </div>
 
                     <div className="w-full bg-[#021B12] p-6 rounded-xl flex items-center justify-between gap-2">
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import { XSPACE_URL } from "@/config/config";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import axios from 'axios';
-import { useEffect, useState } from "react";
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useAccount, useSignMessage, useAccountEffect, useDisconnect } from 'wagmi'
+import { useAccount, useSignMessage } from 'wagmi'
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
     const router = useRouter();
@@ -14,53 +14,27 @@ export default function Home() {
     const { isConnected, address } = useAccount();
     const { data: session, status } = useSession()
     const { signMessageAsync } = useSignMessage();
-    const { data: signMessageData, error, isLoading, signMessage, variables } = useSignMessage()
-
-    const [challenge, setChallenge] = useState(null)
-    const [signature, setSignature] = useState(null)
 
     /**
      * Check if account exists before creating and signing
      * Add next auth
      */
 
-    // const buttonConnectFunc = async () => {
-    //     //Perform button activity
-    //     router.push('/data-space')
-
-    //     const request = await fetch(API_URL.DID_CREATE, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({ sig: '0xF73A2f5E6a56C9f1e6b78137f59b1f9FCA3b0e63', address: '0xF73A2f5E6a56C9f1e6b78137f59b1f9FCA3b0e63' }),
-    //     })
-    // }
-
-    // const fetchToken = async () => {
-    //     const request = await fetch(API_URL.DID_CREATE_MSG + '?' + new URLSearchParams({ address: address }), {
-    //         method: "GET",
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     });
-
-
-    //     const response = await request.json()
-    //     setChallenge(response.msg)
-    // }
+    function sendStorageData(accessToken, refreshToken) {
+        const data = {
+            type: "tokenData",
+            payload: {
+                accessToken: accessToken,
+                refreshToken: refreshToken
+            }
+        };
+        console.log("send data");
+        window.parent.postMessage(data, '*'); // 发送给宿主页面
+    };
 
     const Login = async () => {
         try {
             if (isConnected) {
-                // const response = await fetch(XSPACE_URL.CHALLENGE + '?' + new URLSearchParams({ address: address }), {
-                //     method: "GET",
-                //     headers: {
-                //         'Content-Type': 'application/json',
-                //         'Origin': 'https://xspace.com'
-                //     }
-                // })
-
                 const response = await axios.get(
                     XSPACE_URL.CHALLENGE,
                     {
@@ -101,68 +75,23 @@ export default function Home() {
                             redirect: false, // Set to true if you want to redirect after sign-in
                             token, // Pass the identifier to your provider
                         });
-                        window.location.replace('/data-space')
+                        console.log("access token: ", logResponse.data.accessToken);
+                        console.log("refresh token: ", logResponse.data.refreshToken);
+                        sendStorageData(logResponse.data.accessToken, logResponse.data.refreshToken);
+
+                        // window.location.replace('/data-space')
+                        router.push('/data-space')
                     }
                 }
-            } else {
-                alert("please connect wallet first!")
             }
         } catch (e) {
             alert(e)
         }
     }
 
-    // useEffect(() => {
-    //     if (isConnected && status == "authenticated") {
-    //         window.location.replace('/data-space')
-    //     } if (isConnected && status == "unauthenticated") {
-    //         Login()
-    //     }
-    // }, [isConnected, status, router])
-
-
-    // useEffect(() => {
-    //     const signTx = async (challenge) => {
-    //         await signMessage({ message: challenge })
-    //         await setSignature(signMessageData)
-    //     }
-
-    //     if (address && isConnected && status == "unauthenticated" && signature === null && challenge !== null) {
-    //         signTx(challenge)
-    //     }
-    // }, [challenge])//, isConnected, signature, status, address, signMessage, signMessageData])
-
-    // useEffect(() => {
-    //     const createAPi = async () => {
-    //         const request = await fetch(API_URL.DID_CREATE, {
-    //             method: "POST",
-    //             headers: {
-    //                 'Content-Type': 'application/json'
-    //             },
-    //             body: JSON.stringify({ address: address, sig: signMessageData })
-    //         });
-
-    //         const response = await request.json()
-    //         console.log(response)
-
-    //         const token = response.did
-    //         if (response.did) {
-    //             console.log('Session Key: ', response.did)
-    //             await signIn("credentials", {
-    //                 redirect: false, // Set to true if you want to redirect after sign-in
-    //                 token // Pass the identifier to your provider
-    //             });
-    //         }
-    //         if ('did' in response) {
-    //             //router.push('/data-space')
-    //         }
-    //     }
-
-    //     if (signMessageData !== undefined) {
-    //         console.log("[*] == ", signMessageData)
-    //         createAPi()
-    //     }
-    // }, [signMessageData])
+    useEffect(() => {
+        Login();
+    }, [isConnected])
 
     return (
         <div className="w-full min-h-screen px-8 flex justify-center items-center bg-gradient-to-r from-[#00110A] to-[#000402]">
@@ -171,25 +100,20 @@ export default function Home() {
                 <div className="bg-[#000A06] size-fit -top-[43px] inset-x-0 mx-auto absolute px-4">
                     <Image src={"/images/solar_wallet-broken.svg"} width={86} height={86} alt="" />
                 </div>
-                <h1 className="paytone-one text-3xl mt-4 text-center">Connect Your <span className="text-x-green">Wallet {signature}</span></h1>
+                <h1 className="paytone-one text-3xl mt-4 text-center">Connect Your <span className="text-x-green">Wallet</span></h1>
                 <p className=" text-center my-4">Connect your wallet to create your unique MEMO Identity</p>
                 <ConnectButton.Custom>
                     {({
-                        account,
-                        chain,
-                        openAccountModal,
-                        openChainModal,
                         openConnectModal,
                         authenticationStatus,
-                        mounted,
                     }) => {
                         return (
                             <button onClick={() => {
                                 if (status == "authenticated") {
+                                    alert("authenticated");
                                     signOut();
                                 }
                                 openConnectModal();
-                                Login();
                             }} className="bg-x-green p-2 rounded-full flex gap-2 w-fit mx-auto relative">
                                 <Image src={"/images/clarity_arrow-line.svg"} width={25} height={25} alt="" />
                                 Connect Wallet
